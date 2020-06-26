24538c5517b807204be291177f700d4432646ab4
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-26 01:31:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,7 +131,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages] = ["Assembly"]
+    programmer_hash[:dennis_ritchie][:languages] = "Assembly"
     
     programmer_hash
 end
